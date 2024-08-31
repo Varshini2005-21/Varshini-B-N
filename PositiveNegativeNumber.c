5c2061398706767b74a1ff8a59203ac679cab443
Varshini2005-21 <varshinibn2005@gmail.com>
2024-08-31 12:35:52 +0530

    Add files via upload

---
+++ PositiveNegativeNumber.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+void main() 
+{ int n; 
+ printf("enter the number\n"); 
+ scanf("%d",&n); 
+ if(n<0) 
+ {
+   printf("the number is negative"); 
+ }
+ else if(n==0) 
+ {
+   printf("the number is zero"); 
+ }
+ else
+ {
+   printf("the number is positive");
+ }
+}
